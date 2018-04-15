4c4f7a2d822b2d19a1024120e7c8a2b0f5fa5dc1
Taiwo Oyekanmi <picspacehd@gmail.com>
2018-04-15 17:16:31 -0400

    Added registration functionality

--- server/controllers/authCtrl.js
+++ server/controllers/authCtrl.js
@@ -1,10 +1,63 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/config-constants');
+const User = require('../models/user');
+
+/**
+ *  ============================================ HELPER FUNCTIONS ====================================================
+ */
+
+function _isValidateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function _sendJsonResponse(res, status, content) {
+    res.status(status);
+    res.json(content);
+}
+
+function _generateUserToken(user) {
+    let publicData = User.getPublicInfo(user); // dont allow private data inside the jwt for security purpses
+    return jwt.sign(publicData, config.SECRET, {
+        expiresIn: 86400 * 30 // in seconds
+    });
+}
+// ========================================================
+
 
 /**
  *  ============================================ REQUEST INTERCEPTIONS ====================================================
  */
 
-function register(req, res) {
+function register(req, res, next) {
+    const email = req.body.email;
+    const password = req.body.password;
 
+    if (!email) { return _sendJsonResponse(res, 422, { "message": "No email provided" }); }
+    if (!_isValidateEmail(email)) { return _sendJsonResponse(res, 422, { "message": "Please enter a valid email" }); }
+    if (!password) { return _sendJsonResponse(res, 422, { "message": "No password provided" }); }
+
+    User.findOne({ email: email }, (err, user) => {
+
+        if (err) { return next(err) }
+        if (user) { return _sendJsonResponse(res, 422, { "message": "email already exisits" }); }
+
+        let newUser = new User({
+            email: email,
+            password: password
+        });
+
+        newUser.save((err, user) => {
+
+            if (err) { return next(err) }
+
+            let publicUserInfo = User.getPublicInfo(user);
+            _sendJsonResponse(res, 201, {
+                token: 'JWT ' + _generateUserToken(publicUserInfo),
+                user: publicUserInfo
+            });
+        });
+    });
 };
 
 function login(req, res) {
