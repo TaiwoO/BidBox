4731396f889f49a0701fd4d21424f5fbe5f182b6
Ati Ok <ao10@umbc.edu>
2018-05-03 16:52:06 -0400

    Finished Authorization; TODO: Need to add the styles/themes

--- BidBox/app/src/main/java/finalproject/mobilecomputing/bidbox/Authorization/LoginActivity.java
+++ BidBox/app/src/main/java/finalproject/mobilecomputing/bidbox/Authorization/LoginActivity.java
@@ -1,11 +1,13 @@
 package finalproject.mobilecomputing.bidbox.Authorization;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -14,12 +16,15 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import finalproject.mobilecomputing.bidbox.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button login_Button, goToRegister_Button;
-    private EditText textField_Username, textField_Password;
+    private EditText textField_Email, textField_Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +33,12 @@ public class LoginActivity extends AppCompatActivity {
 
         login_Button = findViewById(R.id.button_login);
         goToRegister_Button = findViewById(R.id.button_goToRegister);
-        textField_Username = findViewById(R.id.text_loginUsername);
+        textField_Email = findViewById(R.id.text_loginEmail);
         textField_Password = findViewById(R.id.text_loginPassword);
 
         // Instantiate the RequestQueue.
         final RequestQueue queue = Volley.newRequestQueue(this);
-        final String url ="http://www.google.com";
+        final String url ="http://631babc0.ngrok.io/auth/login";
 
         // This listener sends an HTTP Requester when clicking the "Login" button.
         login_Button.setOnClickListener(new View.OnClickListener() {
@@ -41,23 +46,37 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // HTTP Request to Sign In.
                 // Request a string response from the provided URL.
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
                         new Response.Listener<String>() {
                             @Override
                             public void onResponse(String response) {
-                                // Display the first 500 characters of the response string.
-//                                mTextView.setText("Response is: "+ response.substring(0,500));
                                 System.out.println(response);
+                                Context context = getApplicationContext();
+                                Toast toast = Toast.makeText(context, response, Toast.LENGTH_LONG);
+                                toast.show();
                             }
-                        }, new Response.ErrorListener() {
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                System.out.println(error);
+                                Context context = getApplicationContext();
+                                Toast toast = Toast.makeText(context, String.valueOf(error), Toast.LENGTH_LONG);
+                                toast.show();
+                            }
+                        }) {
                     @Override
-                    public void onErrorResponse(VolleyError error) {
-//                        mTextView.setText("That didn't work!");
-                        System.out.println(error);
+                    protected Map<String, String> getParams() {
+                        Map<String, String> params = new HashMap<String, String>();
+                        // Get user's input from the TextFields for email and password.
+                        String email = String.valueOf(textField_Email.getText());
+                        String password = String.valueOf(textField_Password.getText());
+                        params.put("email", email);
+                        params.put("password", password);
+                        return params;
                     }
-                });
-
-// Add the request to the RequestQueue.
+                };
+                // Add the request to the RequestQueue.
                 queue.add(stringRequest);
             }
         });
--- BidBox/app/src/main/java/finalproject/mobilecomputing/bidbox/Authorization/RegistrationActivity.java
+++ BidBox/app/src/main/java/finalproject/mobilecomputing/bidbox/Authorization/RegistrationActivity.java
@@ -1,18 +1,30 @@
 package finalproject.mobilecomputing.bidbox.Authorization;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import finalproject.mobilecomputing.bidbox.R;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     private Button register_Button, goToLogin_Button;
-    private EditText textField_registerDisplayName, textField_registerUsername, textField_registerPassword;
+    private EditText textField_registerEmail, textField_registerPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +33,51 @@ public class RegistrationActivity extends AppCompatActivity {
 
         register_Button = findViewById(R.id.button_register);
         goToLogin_Button = findViewById(R.id.button_goToLogin);
-        textField_registerDisplayName = findViewById(R.id.text_registerDisplayName);
-        textField_registerUsername = findViewById(R.id.text_registerUsername);
+        textField_registerEmail = findViewById(R.id.text_registerEmail);
         textField_registerPassword = findViewById(R.id.text_registerPassword);
+
+        // Instantiate the RequestQueue.
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        final String url ="http://631babc0.ngrok.io/auth/register";
 
         register_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // HTTP Request to Sign In.
+                // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                System.out.println(response);
+                                Context context = getApplicationContext();
+                                Toast toast = Toast.makeText(context, response, Toast.LENGTH_LONG);
+                                toast.show();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                System.out.println(error);
+                                Context context = getApplicationContext();
+                                Toast toast = Toast.makeText(context, String.valueOf(error), Toast.LENGTH_LONG);
+                                toast.show();
+                            }
+                        }){
+                    @Override
+                    protected Map<String, String> getParams()
+                    {
+                        Map<String, String>  params = new HashMap<String, String>();
+                        // Get user's input from the TextFields for email and password.
+                        String email = String.valueOf(textField_registerEmail.getText());
+                        String password = String.valueOf(textField_registerPassword.getText());
+                        params.put("email", email);
+                        params.put("password", password);
+                        return params;
+                    }
+                };
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest);
             }
         });
 
@@ -39,6 +89,5 @@ public class RegistrationActivity extends AppCompatActivity {
                 startActivity(loginIntent);
             }
         });
-
     }
 }
