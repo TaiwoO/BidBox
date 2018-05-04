22f2ccaedea57ac5af0f02a7f727e00035599c02
Taiwo Oyekanmi <picspacehd@gmail.com>
2018-05-03 20:50:00 -0400

    Added templates and styling for the home page

--- BidBox/app/src/main/java/finalproject/mobilecomputing/bidbox/HomeActivity.java
+++ BidBox/app/src/main/java/finalproject/mobilecomputing/bidbox/HomeActivity.java
@@ -2,13 +2,47 @@ package finalproject.mobilecomputing.bidbox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import finalproject.mobilecomputing.bidbox.adapters.BidItemAdapter;
+import finalproject.mobilecomputing.bidbox.models.Book;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private List<Book> books;
+    private ListView bidItemListView;
+    private static BidItemAdapter bidItemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        bidItemListView = (ListView)findViewById(R.id.bid_item_listview);
+        books = new ArrayList<>();
+        Book sampleBook = new Book();
+        sampleBook.setIsbn("11112222223333333");
+        sampleBook.setName("Chemistry one");
+        Book sampleBook2 = new Book();
+        sampleBook2.setIsbn("11112222223333333");
+        sampleBook2.setName("Chemistry two");
+        books.add(sampleBook);
+        books.add(sampleBook2);
+        books.add(sampleBook);
+        books.add(sampleBook2);
+        books.add(sampleBook);
+        books.add(sampleBook2);
+        books.add(sampleBook);
+        books.add(sampleBook2);
+        books.add(sampleBook);
+        books.add(sampleBook2);
+
+
+        bidItemAdapter = new BidItemAdapter(this, books);
+        bidItemListView.setAdapter(bidItemAdapter);
     }
 }
---
+++ BidBox/app/src/main/java/finalproject/mobilecomputing/bidbox/adapters/BidItemAdapter.java
@@ -0,0 +1,94 @@
+package finalproject.mobilecomputing.bidbox.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import finalproject.mobilecomputing.bidbox.R;
+import finalproject.mobilecomputing.bidbox.models.Book;
+
+public class BidItemAdapter extends ArrayAdapter<Book> implements View.OnClickListener {
+
+    public static final String TAG = BidItemAdapter.class.getSimpleName();
+    private List<Book> mBooks;
+    Context mContext;
+
+    public BidItemAdapter(@NonNull Context context, @NonNull List<Book> books) {
+        super(context, R.layout.bid_item_row, books);
+
+        this.mBooks = books;
+        this.mContext = context;
+    }
+
+    // View lookup cache
+    private static class ViewHolder {
+        TextView bookName;
+        TextView bookIsbn;
+        ImageView bookImg;
+        Button bidBtn;
+        Button addToChartBtn;
+    }
+
+    @Override
+    public void onClick(View view) {
+        int position = (Integer) view.getTag();
+        Book book = getItem(position);
+
+        switch (view.getId()) {
+            case R.id.bid_item_bid_btn:
+                Log.d(TAG, "OKAYY");
+                Toast.makeText(mContext, "Should go to bid pg now", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.bid_item_addToChart_btn:
+                Toast.makeText(mContext, "Should add the item to the chart now", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        Book book = getItem(position);
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        //
+        ViewHolder viewHolder; // view lookup cache stored in tag
+
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.bid_item_row, parent, false);
+
+            viewHolder.bookName = (TextView) convertView.findViewById(R.id.bid_item_name);
+            viewHolder.bookIsbn = (TextView) convertView.findViewById(R.id.bid_item_isbn);
+            viewHolder.bookImg = (ImageView) convertView.findViewById(R.id.bid_item_img);
+            viewHolder.bidBtn = (Button) convertView.findViewById(R.id.bid_item_bid_btn);
+            viewHolder.addToChartBtn = (Button) convertView.findViewById(R.id.bid_item_addToChart_btn);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.bookName.setText(book.getName());
+        viewHolder.bookIsbn.setText(book.getIsbn());
+        viewHolder.bidBtn.setOnClickListener(this);
+        viewHolder.bidBtn.setTag(position);
+        viewHolder.addToChartBtn.setOnClickListener(this);
+        viewHolder.addToChartBtn.setTag(position);
+
+        return convertView;
+    }
+}
