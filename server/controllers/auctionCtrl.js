61d426bd1bcc8dfa155d90c038d203e307d8d9f4
Taiwo Oyekanmi <picspacehd@gmail.com>
2018-05-14 20:01:21 -0400

    Implemented controller for getting all auctions

--- server/controllers/auctionCtrl.js
+++ server/controllers/auctionCtrl.js
@@ -1,22 +1,36 @@
 const Auction = require('../models/auction');
+
+/**
+ *  ============================================ HELPER FUNCTIONS ====================================================
+ */
+
+
+function _sendJsonResponse(res, status, content) {
+    res.status(status);
+    res.json(content);
+}
+
 /**
  *  ============================================ REQUEST INTERCEPTIONS ====================================================
  */
 
 //TODO:
 function find(req, res) {
-    
+
 };
 
-// TODO:
 function getAll(req, res) {
-
-
-};
-
-//TODO:
-function getAuction(req, res) {
-
+    Auction.find({})
+        .populate("bids")
+        .populate("book")
+        .then((allAuctions) => {
+            console.log("YEET")
+            _sendJsonResponse(res, 200, allAuctions);
+        })
+        .catch((err) => {
+            _sendJsonResponse(res, 404, { "message": "There was an error sorry" });
+            console.log("Error: ", err.message);
+        });
 };
 
 // TODO:
